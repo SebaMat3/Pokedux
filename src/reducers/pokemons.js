e09caef6bb7b18ffe8f4eaea98bc3c5350c0415e
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-04-18 13:38:46 -0300

    feat: Implement ImmutableJS for Redux state
    This commit integrates ImmutableJS to manage state immutability within the Redux store.
    
    - Installs the immutableJS library.
    - Updates the pokemonsReducer to utilize ImmutableJS data structures.
    - Adapts App.js to interact with the ImmutableJS state.

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,39 +1,53 @@
 //src/reducers/pokemons.js
+import { fromJS } from 'immutable';
 
-const initialState = {
+const initialState = fromJS({
     pokemons: [],
     loading: false,
-};
+});
 
 export const pokemonsReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_POKEMONS':
-            return {
-                ...state,
-                pokemons: action.payload,
-            };
+            //return { ...state, pokemons: action.payload};
+            return state.setIn(['pokemons'], fromJS(action.payload));
+
         case 'SET_LOADING':
-            return {
+/*             return {
                 ...state,
                 loading: action.payload,
-            };
+            }; */
+            return state.setIn(['loading'], action.payload);
+
         case 'SET_FAVORITE':
-            const newPokemonsList = [ ...state.pokemons ];
+/*             const newPokemonsList = [ ...state.pokemons ];
             const currentPokemonIndex = newPokemonsList.findIndex(
                 (pokemon) => {
                     return pokemon.id === action.payload.pokemonId;
-            });
+            }); */
+            const currentPokemonIndex = state.get('pokemons').findIndex(
+                (pokemon) => {
+                    return pokemon.get('id') === action.payload.pokemonId;
+            });          
 
             if (currentPokemonIndex < 0) {
                 return state;
             }
+            
+            /* verbose 'get' version
+            const isFavorite = state.get('pokemons').get(currentPokemonIndex).get('favorite'); */
 
+            // short 'getIn' version 
+            const isFavorite = state.getIn(['pokemons', currentPokemonIndex, 'favorite']);
+
+            /* Previous return         
             newPokemonsList[currentPokemonIndex].favorite =
             !newPokemonsList[currentPokemonIndex].favorite;
             return {
                 ...state,
                 pokemons: newPokemonsList,
-            };  
+            };   */
+            return state.setIn(['pokemons', currentPokemonIndex, 'favorite'], !isFavorite);
 
         default:
             return state;
