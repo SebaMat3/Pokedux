c4f829424bc84939f91a22768013b5d931edbc76
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-04-20 21:25:43 -0300

    feat: Migrate to Redux Toolkit for state management
    This commit migrates the application's state management from Redux with ImmerJS to Redux Toolkit.
    Implemented the first slice 'src/slices/dataSlice.js' to manage pokemon data.

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,12 +1,11 @@
 //src/reducers/rootReducers.js
 
-import { combineReducers } from 'redux-immutable';
-import { pokemonsReducer } from './pokemons';
-import { uiReducer } from './ui';
+import { combineReducers } from 'redux';
+import dataReducer from '../slices/dataSlice';
+
 
 const rootReducer = combineReducers({ 
-    data: pokemonsReducer, 
-    ui: uiReducer,
+    data: dataReducer
 });
 
 export default rootReducer;
